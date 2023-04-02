62cfa9dfb79e7c244bfc581c50a02706017fcc37
Nikolas82 <nikobogdan34@gmail.com>
2023-04-02 11:05:26 +0300

    task 11

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,17 +1,19 @@
-import axios from  'axios';
-axios.get()
+import axios from 'axios';
 
+export async function fetchImages(searchImages, page) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '34846253-6b7c00c224b7c5f83c52d1b0f';
 
-
-
-
-// export async function fetchImages(searchImages) {
-//     const url = '?fields=name,capital,population,flags,languages';
-//     return fetch(`https://restcountries.com/v3.1/name/${searchCountry}${url}`)
-//     .then(response => { if (!response.ok) {
-//           throw new Error(response.status);
-//         }
-//         return response.json();
-//       }
-//     )
-//   }
+  try {
+    const response = await axios.get(
+      `${BASE_URL}?key=${API_KEY}&q=${searchImages}&per_page=40&image_type=photo&orientation=horizontal&safesearch=true&page=${page}`
+    );
+    // console.log(response)
+    return {
+      totalHits: response.data.totalHits,
+      hits: response.data.hits,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,95 +3,98 @@ import Notiflix from 'notiflix';
 
 import { fetchImages } from './fetchImages';
 
+const formEl = document.querySelector('#search-form');
+const inputEl = document.querySelector('input[name="searchQuery"]');
+const btnEl = document.querySelector('button[type="submit"]');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
+loadMoreBtn.style.display = 'none';
 
+let searchImages = '';
+let page = 1;
 
+formEl.addEventListener('submit', onSearch);
+loadMoreBtn.addEventListener('click', onLoadMore);
 
+async function onSearch(event) {
+  event.preventDefault();
 
+  searchImages = event.currentTarget.elements.searchQuery.value;
 
+  // console.log(searchImages);
 
+  page = 1;
+  galleryEl.innerHTML = '';
 
+  if (searchImages) {
+    try {
+      const images = await fetchImages(searchImages, page);
 
+      // console.log(images.totalHits);
+      console.log(images.hits);
+      console.log(images);
 
+      if (images.hits.length > 0) {
+        Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images`);
+        loadMoreBtn.style.display = 'block';
+        renderMarkup(images.hits);
+      } else {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
+async function onLoadMore() {
+  if (searchImages) {
+    try {
+      page += 1;
 
+      const images = await fetchImages(searchImages, page);
 
+      renderMarkup(images.hits);
 
+      if (page === 13) {
+        loadMoreBtn.style.display = 'none';
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
-// import { fetchCountries } from './fetchCountries';
+function renderMarkup(images) {
+  const markup = images
+    .map(image => {
+      return `
+<div class="photo-card">
+  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes <br> ${image.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>  Views <br> ${image.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments <br> ${image.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads <br> ${image.downloads}</b>
+    </p>
+  </div>
+</div>
+`;
+    })
+    .join('');
 
-// const DEBOUNCE_DELAY = 300;
-
-// const inputEl = document.querySelector('#search-box');
-// const listEl = document.querySelector('.country-list');
-// const divEl = document.querySelector('.country-info');
-
-// inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
-
-// function onSearch() {
-//   const searchCountry = inputEl.value.trim();
-
-//   if(searchCountry === '' || searchCountry.includes(' ')){
-//     listEl.innerHTML = '';
-//     divEl.innerHTML = '';
-// return;
-//   }
-
-//   fetchCountries(searchCountry)
-//     .then(countries => renderMarkup(countries))
-//     .catch(error => {
-//       console.log(error);
-//       listEl.innerHTML = '';
-//     divEl.innerHTML = '';
-//       Notiflix.Notify.failure('Oops, there is no country with that name');
-//     });
-// }
-
-// function renderMarkup(countries) {
-//   console.log(countries);
-
-//   if (countries.length === 0) {
-//     listEl.innerHTML = '';
-//     divEl.innerHTML = '';
-//   } else if (countries.length === 1) {
-//     const {
-//       name: { official },
-//       flags: { svg },
-//       capital,
-//       population,
-//       languages,
-//     } = countries[0];
-
-//     const countryLanguage = Object.values(languages).join(', ');
-
-//     const markup = `
-//       <h1><img src="${svg}"><span>${official}</span></h1>
-//       <p><b>Capital</b>: ${capital}</p>
-//       <p><b>Population</b>: ${population}</p>
-//       <p><b>Languages</b>: ${countryLanguage}</p>
-//     `;
-
-//     listEl.innerHTML = '';
-//     divEl.innerHTML = markup;
-//   } else if (countries.length >= 2 && countries.length <= 10) {
-//     const markup = countries
-//       .map(({ name: { official }, flags: { svg } }) => {
-//         return `
-//           <li>
-//             <img src="${svg}"  >
-//             <p>${official}</p>
-//           </li>
-//         `;
-//       })
-//       .join('');
-
-//     listEl.innerHTML = markup;
-//     divEl.innerHTML = '';
-//   } else {
-//     listEl.innerHTML = '';
-//     divEl.innerHTML = '';
-//     Notiflix.Notify.info(
-//       'Too many matches found. Please enter a more specific name.'
-//     );
-//   }
-// }
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+}
